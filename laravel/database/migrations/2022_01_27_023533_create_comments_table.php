1e745fa681c5d1c06c520c88a247190c5b7a748f
kentakki416 <fujiken0814@outlook.jp>
2022-01-27 15:11:06 +0900

    投稿APIの作成

---
+++ laravel/database/migrations/2022_01_27_023533_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('photo_id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->text('content');
+            $table->timestamps();
+    
+            $table->foreign('photo_id')->references('id')->on('photos');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
--- laravel/resources/js/store/index.js
+++ laravel/resources/js/store/index.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import auth from './auth'
+import error from './error'
 
 Vue.use(Vuex);
 
 // 種類ごとにモジュールとしてストアを分ける。（認証のデータストア、写真データのデータストア）など
 const store = new Vuex.Store({
   modules: {
-    auth
+    auth,
+    error
   }
 })
 
