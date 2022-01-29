99238c7167810eb3d88ab89a5b3725fecc9b2501
kentakki416 <fujiken0814@outlook.jp>
2022-01-29 18:24:18 +0900

    コメント機能の実装

--- laravel/tests/Feature/PhotoDetailApiTest.php
+++ laravel/tests/Feature/PhotoDetailApiTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Comment;
 use App\Photo;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -15,21 +16,36 @@ class PhotoDetailApiTest extends TestCase
      * @test
      */
     public function should_正しい構造のJSONを返却する()
-    {
-        factory(Photo::class)->create();
-        $photo = Photo::first();
+        {
+            factory(Photo::class)->create()->each(function ($photo) {
+                $photo->comments()->saveMany(factory(Comment::class, 3)->make());
+            });
+            $photo = Photo::first();
 
-        $response = $this->json('GET', route('photo.show', [
-            'id' => $photo->id,
-        ]));
-
-        $response->assertStatus(200)
-            ->assertJsonFragment([
+            $response = $this->json('GET', route('photo.show', [
                 'id' => $photo->id,
-                'url' => $photo->url,
-                'owner' => [
-                    'name' => $photo->owner->name,
-                ],
-            ]);
-    }
+            ]));
+
+            $response->assertStatus(200)
+                ->assertJsonFragment([
+                    'id' => $photo->id,
+                    'url' => $photo->url,
+                    'owner' => [
+                        'name' => $photo->owner->name,
+                    ],
+                    'comments' => $photo->comments
+                        ->sortByDesc('id')
+                        ->map(function ($comment) {
+                            return [
+                                'author' => [
+                                    'name' => $comment->author->name,
+                                ],
+                                'content' => $comment->content,
+                            ];
+                        })
+                        ->all(),
+                ]);
+        }
+
+    
 }
